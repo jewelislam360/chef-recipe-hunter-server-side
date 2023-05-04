4c227e8c2b7c4d6c0bf2f4cd59f439e8df1aeaaf
jewelislam360 <jewelislam.it360@gmail.com>
2023-05-05 00:00:45 +0600

    dynamic data loded

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors')
+const port =process.env.PORT || 5000;
 
+const chef = require('./data.json')
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+
+  app.get('/chef',(req, res)=>{
+    res.send(chef)
+  })
+
+  app.get('/chef/:id',(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef =  chef.find(c=>c.id ===id);
+    res.send(selectedChef);
+    
   })
   
   app.listen(port, () => {
